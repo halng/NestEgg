f24b4084c627184d2477e73d22fcb0aadb074c64
halng <haonguyentan2001@gmail.com>
2026-07-17 21:32:49 +0700

    init(project): add init code for some backend component

---
+++ apps/backend/identity/user/src/main/java/com/nest/egg/identity/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.nest.egg.identity.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
